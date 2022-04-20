145c5c1fe36bc460271405132fb67564ef9013db
Luis Delgado <delgadoluise@hotmail.com>
2022-04-19 18:47:02 -0700

    Cleaned up contactUs textfield

--- ApartafitApp/ContactUsViewController.m
+++ ApartafitApp/ContactUsViewController.m
@@ -8,7 +8,7 @@
 
 @property(nonatomic, readonly) UILabel *introLabel;
 @property(nonatomic, readonly) UITextField *textField;
-@property(nonatomic, readonly) UIButton *signUpButton;
+@property(nonatomic, readonly) UIButton *sendButton;
 
 @end
 
@@ -18,22 +18,32 @@
     self = [super init];
 
     if (self) {
-        _introLabel = [[self class] labelWithText:@"Want to switch up your routine or have any comments for your trainer? Leave a detailed description and we'll get back to you as soon as possible." size:16];
+        _introLabel = [[self class] labelWithText:@"" size:16];
         _introLabel.textColor = UIColor.whiteColor;
+        
+        NSString *text = @"Want to switch up your routine or have any comments for your trainer? Leave a detailed description and we'll get back to you as soon as possible.";
+        NSMutableParagraphStyle *introParagraph = [[NSMutableParagraphStyle alloc] init];
+        introParagraph.alignment = NSTextAlignmentCenter;
+        introParagraph.lineSpacing = 2.0f;
+        introParagraph.lineHeightMultiple = 1.3;
+        NSMutableAttributedString* attrText = [[NSMutableAttributedString alloc] initWithString:text];
+        [attrText addAttribute:NSParagraphStyleAttributeName value:introParagraph range:NSMakeRange(0, text.length)];
+        _introLabel.attributedText = attrText;
+
         [self.view addSubview:_introLabel];
         
         _textField = [[self class] textFieldWithPlaceholderText:@"Description"];
         [self.view addSubview:_textField];
         
-        _signUpButton = [[UIButton alloc] init];
-        [_signUpButton setTitle:@"Sign up now" forState:UIControlStateNormal];
-        _signUpButton.titleLabel.textColor = [UIColor colorWithRed:20/255.0 green:43/255.0 blue:63/255.0 alpha:.5];
-        _signUpButton.translatesAutoresizingMaskIntoConstraints = NO;
-        _signUpButton.backgroundColor = [UIColor colorWithRed:83/255.0 green:147/255.0 blue:115/255.0 alpha:1];
-        [_signUpButton addTarget:self
-                        action:@selector(didTapSignUpButton)
+        _sendButton = [[UIButton alloc] init];
+        [_sendButton setTitle:@"Send" forState:UIControlStateNormal];
+        _sendButton.titleLabel.textColor = [UIColor colorWithRed:20/255.0 green:43/255.0 blue:63/255.0 alpha:.5];
+        _sendButton.translatesAutoresizingMaskIntoConstraints = NO;
+        _sendButton.backgroundColor = [UIColor colorWithRed:83/255.0 green:147/255.0 blue:115/255.0 alpha:1];
+        [_sendButton addTarget:self
+                        action:@selector(didTapSendButton)
               forControlEvents:UIControlEventTouchUpInside];
-        [self.view addSubview:_signUpButton];
+        [self.view addSubview:_sendButton];
     }
 
     return self;
@@ -52,22 +62,16 @@
         [self.introLabel.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:50],
         [self.introLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
         [self.textField.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
-        [self.textField.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor],
+        [self.textField.topAnchor constraintEqualToAnchor:self.introLabel.bottomAnchor constant:10],
         [self.textField.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:.8],
         [self.textField.heightAnchor constraintEqualToConstant:200],
     ]];
     
-    /*
-     //        [moreTextField.leadingAnchor constraintEqualToAnchor:moreLabel.leadingAnchor],
-     //        [moreTextField.topAnchor constraintEqualToAnchor:moreLabel.bottomAnchor constant:8],
-     //        [moreTextField.widthAnchor constraintEqualToAnchor:self.view.widthAnchor constant:-16],
-     */
-    
     [NSLayoutConstraint activateConstraints:@[
-        [self.signUpButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
-        [self.signUpButton.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:.70],
-        [self.signUpButton.heightAnchor constraintEqualToConstant:50],
-        [self.signUpButton.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-65],
+        [self.sendButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [self.sendButton.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:.70],
+        [self.sendButton.heightAnchor constraintEqualToConstant:50],
+        [self.sendButton.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-65],
     ]];
 }
 
@@ -76,6 +80,10 @@
 }
 
 #pragma mark - Private
+
+- (void)didTapSendButton {
+    
+}
 
 + (UILabel *)labelWithText:(NSString *)text size:(CGFloat)size {
     UILabel *label = [[UILabel alloc] init];
